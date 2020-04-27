2ba4bc27294b675818fbf62ca538bc46d9da40d9
Radovan Bartanus <bartanusr@gmail.com>
2020-04-27 10:49:26 +0200

    chore(filter): generated files

--- go-sdk/common/v1/common.pb.go
+++ go-sdk/common/v1/common.pb.go
@@ -79,9 +79,10 @@ func (m *Amount) GetValue() float64 {
 
 // Message representing an expression to be used especially in filter operations
 type Expression struct {
-	Lhs string `protobuf:"bytes,1,opt,name=lhs,proto3" json:"lhs,omitempty"`
-	Op  string `protobuf:"bytes,2,opt,name=op,proto3" json:"op,omitempty"`
-	Rhs string `protobuf:"bytes,3,opt,name=rhs,proto3" json:"rhs,omitempty"`
+	// Operator used in expression
+	Op string `protobuf:"bytes,1,opt,name=op,proto3" json:"op,omitempty"`
+	// Right hand side of the expression - value
+	Rhs string `protobuf:"bytes,2,opt,name=rhs,proto3" json:"rhs,omitempty"`
 }
 
 func (m *Expression) Reset()      { *m = Expression{} }
@@ -116,13 +117,6 @@ func (m *Expression) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Expression proto.InternalMessageInfo
 
-func (m *Expression) GetLhs() string {
-	if m != nil {
-		return m.Lhs
-	}
-	return ""
-}
-
 func (m *Expression) GetOp() string {
 	if m != nil {
 		return m.Op
@@ -137,31 +131,77 @@ func (m *Expression) GetRhs() string {
 	return ""
 }
 
+// List of expressions
+type ExpressionList struct {
+	Items []*Expression `protobuf:"bytes,1,rep,name=items,proto3" json:"items,omitempty"`
+}
+
+func (m *ExpressionList) Reset()      { *m = ExpressionList{} }
+func (*ExpressionList) ProtoMessage() {}
+func (*ExpressionList) Descriptor() ([]byte, []int) {
+	return fileDescriptor_372283428b44e521, []int{2}
+}
+func (m *ExpressionList) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *ExpressionList) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_ExpressionList.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *ExpressionList) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ExpressionList.Merge(m, src)
+}
+func (m *ExpressionList) XXX_Size() int {
+	return m.Size()
+}
+func (m *ExpressionList) XXX_DiscardUnknown() {
+	xxx_messageInfo_ExpressionList.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ExpressionList proto.InternalMessageInfo
+
+func (m *ExpressionList) GetItems() []*Expression {
+	if m != nil {
+		return m.Items
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*Amount)(nil), "common.v1.Amount")
 	proto.RegisterType((*Expression)(nil), "common.v1.Expression")
+	proto.RegisterType((*ExpressionList)(nil), "common.v1.ExpressionList")
 }
 
 func init() { proto.RegisterFile("v1/common.proto", fileDescriptor_372283428b44e521) }
 
 var fileDescriptor_372283428b44e521 = []byte{
-	// 248 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x3c, 0x8f, 0x31, 0x4e, 0xc3, 0x30,
-	0x14, 0x86, 0xfd, 0x52, 0x51, 0x11, 0x0f, 0x80, 0x22, 0x86, 0x88, 0xe1, 0xa9, 0xea, 0xd4, 0x85,
-	0x5a, 0x11, 0x1b, 0x13, 0x20, 0x71, 0x81, 0x8c, 0x6c, 0x69, 0x6a, 0xea, 0x88, 0x24, 0xcf, 0xb2,
-	0xe3, 0x00, 0x1b, 0x47, 0xe0, 0x18, 0x1c, 0x85, 0x31, 0x63, 0x47, 0xe2, 0x2c, 0x8c, 0x3d, 0x02,
-	0x6a, 0x1b, 0xb1, 0xfd, 0x9f, 0xf4, 0x7f, 0xc3, 0xc7, 0xcf, 0xdb, 0x44, 0xe4, 0x54, 0x55, 0x54,
-	0x2f, 0xb5, 0xa1, 0x86, 0xa2, 0x70, 0xa4, 0x36, 0x99, 0xdf, 0xf2, 0xe9, 0x7d, 0x45, 0xae, 0x6e,
-	0xa2, 0x2b, 0x7e, 0x9a, 0x3b, 0x63, 0x64, 0x9d, 0xbf, 0xc7, 0x30, 0x83, 0x45, 0x98, 0xfe, 0x73,
-	0x74, 0xc9, 0x4f, 0xda, 0xac, 0x74, 0x32, 0x0e, 0x66, 0xb0, 0x80, 0xf4, 0x08, 0xf3, 0x3b, 0xce,
-	0x1f, 0xdf, 0xb4, 0x91, 0xd6, 0x16, 0x54, 0x47, 0x17, 0x7c, 0x52, 0x2a, 0x3b, 0xaa, 0xfb, 0x19,
-	0x9d, 0xf1, 0x80, 0xf4, 0x41, 0x09, 0xd3, 0x80, 0xf4, 0xfe, 0x61, 0x94, 0x8d, 0x27, 0xc7, 0x87,
-	0x51, 0xf6, 0x41, 0x76, 0x3d, 0xb2, 0x6d, 0x8f, 0x6c, 0xd7, 0x23, 0x7c, 0x78, 0x84, 0x2f, 0x8f,
-	0xf0, 0xed, 0x11, 0x3a, 0x8f, 0xf0, 0xe3, 0x11, 0x7e, 0x3d, 0xb2, 0x9d, 0x47, 0xf8, 0x1c, 0x90,
-	0x75, 0x03, 0xb2, 0xed, 0x80, 0xec, 0x49, 0x6c, 0x8a, 0x46, 0xb9, 0xd5, 0x32, 0xa7, 0x4a, 0x3c,
-	0x97, 0xf4, 0xea, 0xb4, 0x50, 0x32, 0x5b, 0x97, 0xd2, 0xda, 0x4c, 0x17, 0x56, 0x6c, 0xe8, 0xda,
-	0xae, 0x5f, 0xc6, 0x64, 0xd1, 0x26, 0xab, 0xe9, 0x21, 0xfb, 0xe6, 0x2f, 0x00, 0x00, 0xff, 0xff,
-	0x91, 0x56, 0x9a, 0xd0, 0x09, 0x01, 0x00, 0x00,
+	// 272 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x44, 0x90, 0x31, 0x4b, 0xc3, 0x40,
+	0x18, 0x86, 0xef, 0x6b, 0x69, 0x31, 0x27, 0x54, 0x39, 0x14, 0x82, 0xc3, 0x47, 0xc8, 0x14, 0x10,
+	0x73, 0x44, 0x37, 0xc1, 0x41, 0xc1, 0xcd, 0x29, 0xa3, 0x5b, 0x9a, 0x9e, 0x4d, 0x30, 0xc9, 0x1d,
+	0xb9, 0x24, 0xea, 0xe6, 0x4f, 0xf0, 0x67, 0xf8, 0x53, 0x1c, 0x33, 0x76, 0x34, 0x97, 0xc5, 0xb1,
+	0x3f, 0x41, 0xda, 0x86, 0x76, 0xfb, 0x1e, 0x78, 0xde, 0x8f, 0x97, 0x97, 0x9e, 0x34, 0x01, 0x8f,
+	0x65, 0x9e, 0xcb, 0xc2, 0x57, 0xa5, 0xac, 0x24, 0xb3, 0x06, 0x6a, 0x02, 0xf7, 0x96, 0x4e, 0xef,
+	0x73, 0x59, 0x17, 0x15, 0xbb, 0xa0, 0x47, 0x71, 0x5d, 0x96, 0xa2, 0x88, 0x3f, 0x6c, 0x70, 0xc0,
+	0xb3, 0xc2, 0x3d, 0xb3, 0x33, 0x3a, 0x69, 0xa2, 0xac, 0x16, 0xf6, 0xc8, 0x01, 0x0f, 0xc2, 0x1d,
+	0xb8, 0x3e, 0xa5, 0x8f, 0xef, 0xaa, 0x14, 0x5a, 0xa7, 0xb2, 0x60, 0x33, 0x3a, 0x92, 0x6a, 0x48,
+	0x8e, 0xa4, 0x62, 0xa7, 0x74, 0x5c, 0x26, 0x7a, 0x9b, 0xb0, 0xc2, 0xcd, 0xe9, 0xde, 0xd1, 0xd9,
+	0xc1, 0x7f, 0x4a, 0x75, 0xc5, 0x2e, 0xe9, 0x24, 0xad, 0x44, 0xae, 0x6d, 0x70, 0xc6, 0xde, 0xf1,
+	0xf5, 0xb9, 0xbf, 0x2f, 0xe6, 0x1f, 0xcc, 0x70, 0xe7, 0x3c, 0x88, 0xb6, 0x43, 0xb2, 0xea, 0x90,
+	0xac, 0x3b, 0x84, 0x4f, 0x83, 0xf0, 0x6d, 0x10, 0x7e, 0x0c, 0x42, 0x6b, 0x10, 0x7e, 0x0d, 0xc2,
+	0x9f, 0x41, 0xb2, 0x36, 0x08, 0x5f, 0x3d, 0x92, 0xb6, 0x47, 0xb2, 0xea, 0x91, 0x3c, 0xf3, 0x65,
+	0x5a, 0x25, 0xf5, 0x7c, 0xf3, 0x99, 0xbf, 0x64, 0xf2, 0xad, 0x56, 0x3c, 0x11, 0xd1, 0x22, 0x13,
+	0x5a, 0x47, 0x2a, 0xd5, 0x7c, 0x29, 0xaf, 0xf4, 0xe2, 0x75, 0xd8, 0x87, 0x37, 0xc1, 0x7c, 0xba,
+	0xdd, 0xe8, 0xe6, 0x3f, 0x00, 0x00, 0xff, 0xff, 0xc6, 0x4f, 0x6e, 0x13, 0x36, 0x01, 0x00, 0x00,
 }
 
 func (this *Amount) Equal(that interface{}) bool {
@@ -210,14 +250,40 @@ func (this *Expression) Equal(that interface{}) bool {
 	} else if this == nil {
 		return false
 	}
-	if this.Lhs != that1.Lhs {
-		return false
-	}
 	if this.Op != that1.Op {
 		return false
 	}
 	if this.Rhs != that1.Rhs {
 		return false
+	}
+	return true
+}
+func (this *ExpressionList) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	that1, ok := that.(*ExpressionList)
+	if !ok {
+		that2, ok := that.(ExpressionList)
+		if ok {
+			that1 = &that2
+		} else {
+			return false
+		}
+	}
+	if that1 == nil {
+		return this == nil
+	} else if this == nil {
+		return false
+	}
+	if len(this.Items) != len(that1.Items) {
+		return false
+	}
+	for i := range this.Items {
+		if !this.Items[i].Equal(that1.Items[i]) {
+			return false
+		}
 	}
 	return true
 }
@@ -236,11 +302,22 @@ func (this *Expression) GoString() string {
 	if this == nil {
 		return "nil"
 	}
-	s := make([]string, 0, 7)
+	s := make([]string, 0, 6)
 	s = append(s, "&v1.Expression{")
-	s = append(s, "Lhs: "+fmt.Sprintf("%#v", this.Lhs)+",\n")
 	s = append(s, "Op: "+fmt.Sprintf("%#v", this.Op)+",\n")
 	s = append(s, "Rhs: "+fmt.Sprintf("%#v", this.Rhs)+",\n")
+	s = append(s, "}")
+	return strings.Join(s, "")
+}
+func (this *ExpressionList) GoString() string {
+	if this == nil {
+		return "nil"
+	}
+	s := make([]string, 0, 5)
+	s = append(s, "&v1.ExpressionList{")
+	if this.Items != nil {
+		s = append(s, "Items: "+fmt.Sprintf("%#v", this.Items)+",\n")
+	}
 	s = append(s, "}")
 	return strings.Join(s, "")
 }
@@ -313,21 +390,51 @@ func (m *Expression) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		copy(dAtA[i:], m.Rhs)
 		i = encodeVarintCommon(dAtA, i, uint64(len(m.Rhs)))
 		i--
-		dAtA[i] = 0x1a
+		dAtA[i] = 0x12
 	}
 	if len(m.Op) > 0 {
 		i -= len(m.Op)
 		copy(dAtA[i:], m.Op)
 		i = encodeVarintCommon(dAtA, i, uint64(len(m.Op)))
 		i--
-		dAtA[i] = 0x12
-	}
-	if len(m.Lhs) > 0 {
-		i -= len(m.Lhs)
-		copy(dAtA[i:], m.Lhs)
-		i = encodeVarintCommon(dAtA, i, uint64(len(m.Lhs)))
-		i--
 		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *ExpressionList) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ExpressionList) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *ExpressionList) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Items) > 0 {
+		for iNdEx := len(m.Items) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Items[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintCommon(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0xa
+		}
 	}
 	return len(dAtA) - i, nil
 }
@@ -365,10 +472,6 @@ func (m *Expression) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = len(m.Lhs)
-	if l > 0 {
-		n += 1 + l + sovCommon(uint64(l))
-	}
 	l = len(m.Op)
 	if l > 0 {
 		n += 1 + l + sovCommon(uint64(l))
@@ -376,6 +479,21 @@ func (m *Expression) Size() (n int) {
 	l = len(m.Rhs)
 	if l > 0 {
 		n += 1 + l + sovCommon(uint64(l))
+	}
+	return n
+}
+
+func (m *ExpressionList) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if len(m.Items) > 0 {
+		for _, e := range m.Items {
+			l = e.Size()
+			n += 1 + l + sovCommon(uint64(l))
+		}
 	}
 	return n
 }
@@ -402,9 +520,23 @@ func (this *Expression) String() string {
 		return "nil"
 	}
 	s := strings.Join([]string{`&Expression{`,
-		`Lhs:` + fmt.Sprintf("%v", this.Lhs) + `,`,
 		`Op:` + fmt.Sprintf("%v", this.Op) + `,`,
 		`Rhs:` + fmt.Sprintf("%v", this.Rhs) + `,`,
+		`}`,
+	}, "")
+	return s
+}
+func (this *ExpressionList) String() string {
+	if this == nil {
+		return "nil"
+	}
+	repeatedStringForItems := "[]*Expression{"
+	for _, f := range this.Items {
+		repeatedStringForItems += strings.Replace(f.String(), "Expression", "Expression", 1) + ","
+	}
+	repeatedStringForItems += "}"
+	s := strings.Join([]string{`&ExpressionList{`,
+		`Items:` + repeatedStringForItems + `,`,
 		`}`,
 	}, "")
 	return s
@@ -544,38 +676,6 @@ func (m *Expression) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Lhs", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowCommon
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthCommon
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthCommon
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Lhs = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Op", wireType)
 			}
 			var stringLen uint64
@@ -606,7 +706,7 @@ func (m *Expression) Unmarshal(dAtA []byte) error {
 			}
 			m.Op = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
-		case 3:
+		case 2:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Rhs", wireType)
 			}
@@ -637,6 +737,93 @@ func (m *Expression) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.Rhs = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipCommon(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthCommon
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthCommon
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *ExpressionList) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowCommon
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ExpressionList: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ExpressionList: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Items", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowCommon
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthCommon
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthCommon
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Items = append(m.Items, &Expression{})
+			if err := m.Items[len(m.Items)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
